6e8d97185357fa5c3c7d31777b477be045be24e5
Mitsamay Keotheuankham <mitsamay@gmail.com>
2023-08-07 11:31:23 +0700

    07-08-2023

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   AppBar,
   Box,
@@ -7,65 +7,69 @@ import {
   IconButton,
   Toolbar,
   Typography,
-} from '@mui/material';
-import { Lock, Menu } from '@mui/icons-material';
+} from "@mui/material";
+import { Lock, Menu } from "@mui/icons-material";
+import photoURL from "../profile.png";
+import UserIcons from "./user/UserIcons";
+import { useValue } from "../context/ContextProvider";
 
-// import { useValue } from '../context/ContextProvider';
-// import UserIcons from './user/UserIcons';
+const user = { name: "test", photoURL };
+
 // import Sidebar from './sidebar/Sidebar';
 
 const NavBar = () => {
-//   const {
-//     state: { currentUser },
-//     dispatch,
-//   } = useValue();
+  const {
+    state: { currentUser },
+    dispatch,
+  } = useValue();
 
-//   const [isOpen, setIsOpen] = useState(false);
+  //   const [isOpen, setIsOpen] = useState(false);
 
   return (
     // <>
-      <AppBar>
-        <Container maxWidth="lg">
-          <Toolbar disableGutters>
-            <Box sx={{ mr: 1 }}>
-              <IconButton
-                size="large"
-                color="inherit"
-                // onClick={() => setIsOpen(true)}
-              >
-                <Menu />
-              </IconButton>
-            </Box>
-            <Typography
-              variant="h6"
-              component="h1"
-              noWrap
-              sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+    <AppBar>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Box sx={{ mr: 1 }}>
+            <IconButton
+              size="large"
+              color="inherit"
+              // onClick={() => setIsOpen(true)}
             >
-              Mitsamay System
-            </Typography>
-            <Typography
-              variant="h6"
-              component="h1"
-              noWrap
-              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+              <Menu />
+            </IconButton>
+          </Box>
+          <Typography
+            variant="h6"
+            component="h1"
+            noWrap
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+          >
+            Mitsamay System
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h1"
+            noWrap
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            MSM
+          </Typography>
+          {!currentUser ? (
+            <Button
+              color="inherit"
+              startIcon={<Lock />}
+              // onClick={() => dispatch({ type: 'OPEN_LOGIN' })}
+              onClick={() => dispatch({ type: "UPDATE_USER", payload: user })}
             >
-              MSM
-            </Typography>
-            {/* {!currentUser ? (
-              <Button
-                color="inherit"
-                startIcon={<Lock />}
-                onClick={() => dispatch({ type: 'OPEN_LOGIN' })}
-              >
-                Login
-              </Button>
-            ) : (
-              <UserIcons />
-            )} */}
-          </Toolbar>
-        </Container>
-      </AppBar>
+              Login
+            </Button>
+          ) : (
+            <UserIcons />
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
     //   {/* <Toolbar /> */}
     //   {/* <Sidebar {...{ isOpen, setIsOpen }} /> */}
     // </>
