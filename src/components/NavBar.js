a6825e11ef61488e41bdfb760330bf22c4d480c2
Mitsamay Keotheuankham <mitsamay@gmail.com>
2023-08-16 20:40:53 +0700

    16-08-2023

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -42,7 +42,7 @@ const NavBar = () => {
               noWrap
               sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
             >
-              You Are Welcome
+              Mitsamay System
             </Typography>
             <Typography
               variant="h6"
