7cd112f47885da15949d6a8888138d29f6a95d3c
Mitsamay Keotheuankham <mitsamay@gmail.com>
2023-08-16 09:03:03 +0700

    16-08-2023

--- src/actions/room.js
+++ src/actions/room.js
@@ -1,4 +1,4 @@
-// import deleteImages from './utils/deleteImages';
+import deleteImages from './utils/deleteImages';
 import fetchData from "./utils/fetchData";
 
 const url = process.env.REACT_APP_SERVER_URL + "/room";
@@ -38,26 +38,27 @@ export const getRooms = async (dispatch) => {
   }
 };
 
-// export const deleteRoom = async (room, currentUser, dispatch) => {
-//   dispatch({ type: 'START_LOADING' });
+//-----------------------------------------------------------------
+export const deleteRoom = async (room, currentUser, dispatch) => {
+  dispatch({ type: 'START_LOADING' });
 
-//   const result = await fetchData(
-//     { url: `${url}/${room._id}`, method: 'DELETE', token: currentUser?.token },
-//     dispatch
-//   );
-//   if (result) {
-//     dispatch({
-//       type: 'UPDATE_ALERT',
-//       payload: {
-//         open: true,
-//         severity: 'success',
-//         message: 'The room has been deleted successfully',
-//       },
-//     });
+  const result = await fetchData(
+    { url: `${url}/${room._id}`, method: 'DELETE', token: currentUser?.token },
+    dispatch
+  );
+  if (result) {
+    dispatch({
+      type: 'UPDATE_ALERT',
+      payload: {
+        open: true,
+        severity: 'success',
+        message: 'The room has been deleted successfully',
+      },
+    });
 
-//     dispatch({ type: 'DELETE_ROOM', payload: result._id });
-//     deleteImages(room.images, currentUser.id);
-//   }
+    dispatch({ type: 'DELETE_ROOM', payload: result._id });
+    deleteImages(room.images, currentUser.id);
+  }
 
-//   dispatch({ type: 'END_LOADING' });
-// };
+  dispatch({ type: 'END_LOADING' });
+};
