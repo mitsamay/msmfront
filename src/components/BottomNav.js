d07f6e448782e2d7c7922679642c5cf63d8b571a
Mitsamay Keotheuankham <mitsamay@gmail.com>
2023-08-16 21:06:45 +0700

    Update BottomNav.js

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -3,60 +3,51 @@ import {
   BottomNavigationAction,
   Box,
   Paper,
-} from "@mui/material";
-import { AddLocationAlt, Bed, LocationOn, Approval } from "@mui/icons-material";
-import { useEffect, useRef, useState } from "react";
-import ClusterMap from "./map/ClusterMap";
-import Rooms from "./rooms/Rooms";
-import AddRoom from "./addRoom/AddRoom";
-import Deeds from "./deeds/Deeds";
-import AddDeeds from "./addDeeds/AddDeeds";
-import Protected from "./protected/Protected";
-import { useValue } from "../context/ContextProvider";
+} from '@mui/material';
+import { AddLocationAlt, Bed, LocationOn } from '@mui/icons-material';
+import { useEffect, useRef, useState } from 'react';
+import ClusterMap from './map/ClusterMap';
+import Rooms from './rooms/Rooms';
+import AddRoom from './addRoom/AddRoom';
+import Protected from './protected/Protected';
+import { useValue } from '../context/ContextProvider';
 
 const BottomNav = () => {
   const {
     state: { section },
     dispatch,
   } = useValue();
-
   const ref = useRef();
-
   useEffect(() => {
     ref.current.ownerDocument.body.scrollTop = 0;
   }, [section]);
-
   return (
     <Box ref={ref}>
       {
         {
           0: <ClusterMap />,
           1: <Rooms />,
-          2: <Deeds />,
-          3: (
+          2: (
             <Protected>
               <AddRoom />
             </Protected>
           ),
-          4: <AddDeeds />,
         }[section]
       }
       <Paper
         elevation={3}
-        sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 2 }}
+        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 2 }}
       >
         <BottomNavigation
           showLabels
           value={section}
           onChange={(e, newValue) =>
-            dispatch({ type: "UPDATE_SECTION", payload: newValue })
+            dispatch({ type: 'UPDATE_SECTION', payload: newValue })
           }
         >
-          <BottomNavigationAction label="Map" icon={<LocationOn />} />
-          <BottomNavigationAction label="Rooms" icon={<Bed />} />
-          <BottomNavigationAction label="Deeds" icon={<Approval />} />
-          <BottomNavigationAction label="Add Rooms" icon={<AddLocationAlt />} />
-          <BottomNavigationAction label="Add Deeds" icon={<AddLocationAlt />} />
+          <BottomNavigationAction label="Map ແຜນທີ່" icon={<LocationOn />} />
+          <BottomNavigationAction label="Rooms ຫ້ອງຊຸດ" icon={<Bed />} />
+          <BottomNavigationAction label="Add ເພີ້ມຂໍ້ມູນ" icon={<AddLocationAlt />} />
         </BottomNavigation>
       </Paper>
     </Box>
