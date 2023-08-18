48b0838e0ed97a34e805fe4ecedc4726384d9821
Mitsamay Keotheuankham <mitsamay@gmail.com>
2023-08-18 20:27:14 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,12 @@ import Login from '../components/user/Login';
 
 const Home = () => {
   return (
-    <>
+    <div style={{ width: '100%', height: '80%' }}>
       <Login />
       <NavBar />
       <BottomNav />
-    </>
+    </div>
+    
   );
 };
 
