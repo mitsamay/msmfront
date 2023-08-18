16cf80d466d535a69f005d8a65b0e8780969dc04
Mitsamay Keotheuankham <mitsamay@gmail.com>
2023-08-18 23:52:18 +0700

    18-08-2023

--- src/components/map/GisMap.js
+++ src/components/map/GisMap.js
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactMapGL, {
   NavigationControl,
   FullscreenControl,
   ScaleControl,
   GeolocateControl,
 } from "react-map-gl";
-// import MapboxDraw from "@mapbox/mapbox-gl-draw";
-// import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 
 const GisMap = () => {
   return (
-    // <div>GisMap ກໍາລັງດໍາເນີນການສ້າງ</div>
+    <>
     <ReactMapGL
-      initialViewState={{ latitude: 18.099, longitude: 102.669, zoom: 5 }}
+      initialViewState={{ latitude: 18.099, longitude: 103.669, zoom: 5 }}
       mapboxAccessToken={process.env.REACT_APP_MAP_TOKEN}
       mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
     >
       <GeolocateControl position="top-left" />
       <FullscreenControl position="top-left" />
       <NavigationControl position="top-left" />
+      <useControl />
       <ScaleControl />
     </ReactMapGL>
+    </>
   );
 };
 
---
+++ src/components/map/RulerControl/labelFormat.js
@@ -0,0 +1,8 @@
+export default function labelFormat(number) {
+    if (number < 1) {
+      return `${(number * 1000).toFixed()} m`
+    }
+  
+    return `${number.toFixed(2)} km`
+  }
+  
